3c7f13ed523f8d53cb3f910953d14a4a510d7eda
Marius Kluften <mkluften93@gmail.com>
2023-09-12 15:13:52 +0200

    drag out header from each companys modal content

---
+++ src/lib/components/companies/CompanyHeader.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	export let imgSrc: string;
+	export let handleClose: () => void;
+	export let href: string;
+</script>
+
+<div class="relative">
+	<h3 class="about-company-header relative">
+		<a {href} target="_blank">
+			<img src={imgSrc} alt="" class="w-24 m-0" />
+		</a>
+	</h3>
+	<button class="absolute -right-4 -top-4" on:click={handleClose}>X</button>
+</div>
