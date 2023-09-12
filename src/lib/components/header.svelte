a2fc082478404f6d87546dd39b728930e5ed2009
Marius Kluften <mkluften93@gmail.com>
2023-09-12 15:15:19 +0200

    add header component

---
+++ src/lib/components/header.svelte
@@ -0,0 +1,17 @@
+<header
+	class="flex md:flex-row flex-col justify-between items-center md:items-end md:m-16 max-w-7xl"
+>
+	<a href="/">
+		<!-- TODO: Replace with local asset -->
+		<img src="https://www.overhuset.no/images/overhuset_logo.svg" class="w-44" alt="" />
+	</a>
+	<nav>
+		<ul class="flex gap-4 md:items-end text-sm">
+			<li><a href="/">Hjem</a></li>
+			<li><a href="/#selskapene">Selskapene</a></li>
+			<li><a href="/#om_oss">Om oss</a></li>
+			<li><a href="/kompetanse">Kompetanse</a></li>
+			<li><a href="/#kontakt">Kontakt</a></li>
+		</ul>
+	</nav>
+</header>
