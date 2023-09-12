2700dc0ae0cf421e5c1fade9e70992a57eb24444
Marius Kluften <mkluften93@gmail.com>
2023-09-12 15:15:31 +0200

    add shared layout component

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/header.svelte';
+	import '../app.css';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+
+	initializeStores();
+</script>
+
+<Header />
+<slot />
+<Footer />
