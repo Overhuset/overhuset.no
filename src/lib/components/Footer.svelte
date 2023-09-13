0f86bfeb33d6a87b85473b73b4eb0ebe8be521b8
Marius Kluften <mkluften93@gmail.com>
2023-09-13 23:23:58 +0200

    fix some styles

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,25 +1,23 @@
-<footer id="kontakt" class="bg-[#A6C4BC] p-12 md:p-20">
+<footer id="kontakt" class="bg-[#A6C4BC] p-12 md:p-20 mt-auto">
 	<div class="max-w-8xl mx-auto">
 		<h2 class="text-6xl mb-6 text-white header-decoration">KONTAKT</h2>
 		<a href="/">
 			<!-- TODO: Replace with local asset -->
 			<img src="https://www.overhuset.no/images/overhuset_logo.svg" class="w-44 mb-4" alt="" />
 		</a>
-		<a
-			href="https://www.google.no/maps/place/Munkedamsveien+3B,+0161+Oslo/@59.9144348,10.7292813,17z/data=!3m1!4b1!4m5!3m4!1s0x46416e7e111e7bb7:0x33191f0728977b46!8m2!3d59.9144348!4d10.7314753"
-			target="map"
-			class="font-bold"
-		>
-			Munkedamsveien 3b, 0161 OSLO
-		</a>
-		<br />
-		<br />
-		<p>
-			<a href="tel:+4792036802">+47 920 36 802</a><br />
+		<span class="flex flex-col">
+			<a
+				href="https://www.google.no/maps/place/Munkedamsveien+3B,+0161+Oslo/@59.9144348,10.7292813,17z/data=!3m1!4b1!4m5!3m4!1s0x46416e7e111e7bb7:0x33191f0728977b46!8m2!3d59.9144348!4d10.7314753"
+				target="map"
+				class="font-bold"
+			>
+				Munkedamsveien 3b, 0161 OSLO
+			</a>
+			<a href="tel:+4792036802">+47 920 36 802</a>
 			<a href="mailto:ars.johnsen@overhuset.no">lars.johnsen@overhuset.no</a>
-		</p>
+		</span>
 
-		<p class="text-right">
+		<p class="md:text-right">
 			All rights reserved.<br />
 			© 2023 Overhuset
 		</p>
