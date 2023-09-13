8a5ab7cb6e819c7bec6636175e1dc2c0928f17e2
Marius Kluften <mkluften93@gmail.com>
2023-09-13 13:39:31 +0200

    dummy page for events

---
+++ src/routes/kompetanse/+page.ts
@@ -0,0 +1,28 @@
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import { parse } from 'papaparse';
+
+export async function load({ params }) {
+	const events = await getEventsFromSheet();
+	return {
+		data: [
+			{
+				date: '2022-19-10',
+				company: 'Overhuset'
+			}
+		],
+		events
+	};
+}
+async function getEventsFromSheet() {
+	const sheet_id =
+		'2PACX-1vRHWRCeRufO1C5jOtbqorHJtqFVB2UGGnm6_AXgcVk2LV2Br_loJmnxyS5SLw19VgvqKXnON6BigYX0';
+	const response = await fetch(
+		`https://docs.google.com/spreadsheets/d/e/${sheet_id}/pub?gid=257492918&single=true&output=csv`
+	);
+	const csv = await response.text();
+	const json = parse(csv, {
+		header: true
+	});
+	return json.data;
+}
