629b55d540019d9319875b811716a1f93669de2a
Marius Kluften <mkluften93@gmail.com>
2023-09-14 21:13:38 +0200

    clean up and move accordion to be parent of all accordion items

--- src/lib/components/EventItem.svelte
+++ src/lib/components/EventItem.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { Accordion, AccordionItem } from '@skeletonlabs/skeleton';
+	import { AccordionItem } from '@skeletonlabs/skeleton';
 	import { getLogoFromCompanyName } from '$lib/components/companies';
 	import type { Event } from '$lib/types';
 	export let event: Event;
@@ -8,44 +8,37 @@
 	const logo = getLogoFromCompanyName(event.company);
 </script>
 
-<Accordion>
-	<div class="bg-stone-100 rounded-xl w-full max-w-sm">
-		<AccordionItem>
-			<!-- <svelte:fragment slot="lead"> -->
-			<!-- 	<img src="https://www.overhuset.no/images/companies/logo_jpro.png" alt="" class="w-20 h-40" /> -->
-			<!-- </svelte:fragment> -->
-			<svelte:fragment slot="summary">
-				<div class="flex items-center w-full">
-					<img src={logo} alt="" class="w-20" />
-					<div class="flex flex-col border-stone-600 p-4 font-source">
-						<span class="font-light text-sm">Tor, sep 14 2023 - 17:30</span>
-						<span class="font-bold">{event?.title ?? 'Sveltekit fagdag'}</span>
-						<span class="font-light text-sm">
-							{event?.company && event?.location
-								? `${event?.company}, ${event?.location}`
-								: 'Oslo @ Rebel'}
-						</span>
-					</div>
-				</div>
-			</svelte:fragment>
-			<svelte:fragment slot="content">
-				<div class="px-4 pb-2 space-y-8">
-					<p class="prose">
-						{event?.description ??
-							'Nå er det på tide med enda en lorem ipsum sim dum ting tar lang tid'}
-					</p>
-					<p class="flex justify-end gap-2">
-						{isInThePast ? 'Se presentasjon her:' : 'Påmelding:'}
-						<a
-							href={(isInThePast ? event?.presentationLink : event?.registrationLink) ??
-								'https://vg.no/'}
-							class="underline font-bold"
-						>
-							Trykk her
-						</a>
-					</p>
-				</div>
-			</svelte:fragment>
-		</AccordionItem>
-	</div>
-</Accordion>
+<AccordionItem class="bg-stone-100 rounded-xl w-full max-w-sm">
+	<svelte:fragment slot="summary">
+		<div class="flex items-center w-full">
+			<img src={logo} alt="" class="w-20" />
+			<div class="flex flex-col pl-4 py-4 font-source">
+				<span class="font-light text-sm">{event?.date ?? 'TBA'}</span>
+				<span class="font-bold whitespace-nowrap">{event?.title ?? 'Sveltekit fagdag'}</span>
+				<span class="font-light text-sm">
+					{event?.company && event?.location
+						? `${event?.company}, ${event?.location}`
+						: 'Oslo @ Rebel'}
+				</span>
+			</div>
+		</div>
+	</svelte:fragment>
+	<svelte:fragment slot="content">
+		<div class="px-4 pb-2 space-y-8">
+			<p class="prose">
+				{event?.description ??
+					'Nå er det på tide med enda en lorem ipsum sim dum ting tar lang tid'}
+			</p>
+			<p class="flex justify-end gap-2">
+				{isInThePast ? 'Se presentasjon her:' : 'Påmelding:'}
+				<a
+					href={(isInThePast ? event?.presentationLink : event?.registrationLink) ??
+						'https://vg.no/'}
+					class="underline font-bold"
+				>
+					Trykk her
+				</a>
+			</p>
+		</div>
+	</svelte:fragment>
+</AccordionItem>
--- src/lib/components/EventItemList.svelte
+++ src/lib/components/EventItemList.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
 	import EventItem from './EventItem.svelte';
 	import type { Event } from '$lib/types';
+	import { Accordion } from '@skeletonlabs/skeleton';
 
 	export let events: Event[];
 	export let isInThePast = false;
 </script>
 
-<ul class="grid md:grid-cols-2 gap-x-4 gap-y-2">
-	{#each events as event}
-		<EventItem {event} {isInThePast} />
-	{/each}
-</ul>
+<Accordion>
+	<ul class="grid md:grid-cols-2 gap-x-4 gap-y-2 items-start">
+		{#each events as event}
+			<EventItem {event} {isInThePast} />
+		{/each}
+	</ul>
+</Accordion>
