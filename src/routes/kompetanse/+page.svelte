56347f850cf13ae56e04fecbcc5d88d5f3c76696
Marius Kluften <mkluften93@gmail.com>
2023-09-14 17:46:46 +0200

    cleanup and fix style on tabs

--- src/routes/kompetanse/+page.svelte
+++ src/routes/kompetanse/+page.svelte
@@ -1,23 +1,19 @@
 <script lang="ts">
-	import EventItem from '$lib/components/EventItem.svelte';
 	import EventItemList from '$lib/components/EventItemList.svelte';
 	import { Tab, TabGroup } from '@skeletonlabs/skeleton';
 
 	export let data;
 	let tabSet = 0;
-	console.log(data);
 	const pastEvents = data.events.slice(0, 10);
 	const futureEvents = data.events.slice(9, -1);
-
-	console.log(pastEvents, futureEvents);
 </script>
 
 <section class="max-w-8xl mx-auto">
-	<TabGroup class="w-full" justify="justify-around" border="" active="bg-stone-100 !rounded-t-lg">
-		<Tab bind:group={tabSet} name="past" value={0}>
+	<TabGroup class="w-full" justify="justify-around" active="bg-stone-100 !rounded-t-lg">
+		<Tab bind:group={tabSet} name="past" value={0} class="w-1/2">
 			<svelte:fragment slot="lead">Tidligere samlinger</svelte:fragment>
 		</Tab>
-		<Tab bind:group={tabSet} name="future" value={1}>
+		<Tab bind:group={tabSet} name="future" value={1} class="w-1/2">
 			<svelte:fragment slot="lead">Framtidige samlinger</svelte:fragment>
 		</Tab>
 		<svelte:fragment slot="panel">
