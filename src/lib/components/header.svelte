f8dec331cb422209fa5ff661b0bd984988c3b4e1
Marius Kluften <mkluften93@gmail.com>
2023-09-19 13:32:09 +0200

    add intro text

--- src/lib/components/header.svelte
+++ src/lib/components/header.svelte
@@ -1,5 +1,5 @@
 <header
-	class="flex md:flex-row flex-col justify-between items-center gap-6 md:items-end mt-16 md:mt-24 mb-12 mx-auto max-w-8xl w-full"
+	class="flex md:flex-row flex-col justify-between items-center gap-6 md:items-end mt-16 md:mt-24 mb-12 mx-auto max-w-8xl w-full md:px-12"
 >
 	<a href="/" title="Hjem">
 		<!-- TODO: Replace with local asset -->
--- src/routes/kompetanse/+page.svelte
+++ src/routes/kompetanse/+page.svelte
@@ -9,6 +9,15 @@
 </script>
 
 <section class="max-w-8xl mx-auto">
+	<div class="prose mb-16 mt-8 mx-4">
+		<h1>Fagarrangement</h1>
+		<p>
+			Her kan du få et innblikk i de forskjellige aktivitetene, arrangementene og hendelsene
+			Overhuset gjennomfører i løpet av året. Vi legger vekt på å dele kunnskap, inspirere og
+			engasjere.
+		</p>
+		<p>Ta en titt og se hva som skjer hos oss!</p>
+	</div>
 	<TabGroup
 		class="w-full"
 		padding="pt-2 pb-1 px-4"
