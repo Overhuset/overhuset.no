1ad6e67a98e0ef5f27237a2b63583aa509f21927
Marius Kluften <mkluften93@gmail.com>
2023-09-19 15:01:24 +0200

    remove event logic from component and make it purely a view

--- src/routes/kompetanse/+page.server.ts
+++ src/routes/kompetanse/+page.server.ts
@@ -1,49 +1,11 @@
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import Papa from 'papaparse';
-
 import { SHEET_ID } from '$env/static/private';
 import { dev } from '$app/environment';
-import type { Event } from '$lib/types';
-import { isEventInThePast, sortEvent, transformDates } from '$lib/transformEvents';
+import { getEventsFromSheet } from '$lib/getEventsFromSheet';
 
 export async function load() {
-	const events = await getEventsFromSheet();
+	const events = await getEventsFromSheet(SHEET_ID);
 	return {
 		...events
-	};
-}
-
-async function getEventsFromSheet() {
-	const response = await fetch(
-		`https://docs.google.com/spreadsheets/d/e/${SHEET_ID}/pub?gid=257492918&single=true&output=csv`
-	);
-	const csv = await response.text();
-	const json = Papa.parse(csv, {
-		header: true
-	});
-
-	if (json?.data) {
-		const events = json?.data as Event[];
-		const pastEvents = events
-			.filter((event: Event) => isEventInThePast(event.date, event.time))
-			.sort((a, b) => sortEvent(a, b, true));
-		const futureEvents = events
-			.filter((event: Event) => !isEventInThePast(event.date, event.time))
-			.sort(sortEvent);
-
-		const pastEventsWithTransformedDates = transformDates(pastEvents);
-		const futureEventsWithTransformedDates = transformDates(futureEvents);
-
-		return {
-			pastEvents: pastEventsWithTransformedDates,
-			futureEvents: futureEventsWithTransformedDates
-		};
-	}
-
-	return {
-		pastEvents: [],
-		futureEvents: []
 	};
 }
 
--- src/routes/kompetanse/+page.svelte
+++ src/routes/kompetanse/+page.svelte
@@ -1,9 +1,7 @@
 <script lang="ts">
-	import EventItemList from '$lib/components/EventItemList.svelte';
-	import { Tab, TabGroup } from '@skeletonlabs/skeleton';
+	import Events from '$lib/components/Events.svelte';
 
 	export let data;
-	let tabSet = 1;
 	const pastEvents = data.pastEvents;
 	const futureEvents = data.futureEvents;
 </script>
@@ -18,25 +16,5 @@
 		</p>
 		<p>Ta en titt og se hva som skjer hos oss!</p>
 	</div>
-	<TabGroup
-		class="w-full"
-		padding="pt-2 pb-1 px-4"
-		rounded="rounded-t-lg"
-		justify="justify-around"
-		active="bg-[#3a2417] text-white"
-	>
-		<Tab bind:group={tabSet} name="past" value={0} class="w-1/2">
-			<svelte:fragment slot="lead">Tidligere samlinger</svelte:fragment>
-		</Tab>
-		<Tab bind:group={tabSet} name="future" value={1} class="w-1/2">
-			<svelte:fragment slot="lead">Framtidige samlinger</svelte:fragment>
-		</Tab>
-		<svelte:fragment slot="panel">
-			{#if tabSet === 0}
-				<EventItemList events={pastEvents} isInThePast={true} />
-			{:else}
-				<EventItemList events={futureEvents} />
-			{/if}
-		</svelte:fragment>
-	</TabGroup>
+	<Events {pastEvents} {futureEvents} />
 </section>
