979ea1774b1ead11145f58699e715f424c0a4be6
Marius Kluften <mkluften93@gmail.com>
2023-09-19 11:04:54 +0200

    add sannsyn text

--- src/lib/components/companies/Sannsyn.svelte
+++ src/lib/components/companies/Sannsyn.svelte
@@ -8,19 +8,34 @@
 <div class="prose">
 	<CompanyHeader {href} {imgSrc} />
 	<p>
-		Sannsyn er et av Norges ledende fagmiljøer innen Data Science og maskinlæring. Vi hjelper privat
-		og offentlig sektor å skape konkurransekraft med egne data.<br /><br /> Sannsyn ble etablert i
-		2012, noe som gjør oss til et av Norges mest erfarne fagmiljøer innen maskinlæring og data
-		science. Inntil 73 prosent av tilgjengelig data blir aldri brukt på en god strategisk måte og
-		slik bør det ikke være.<br /><br /> Ved å hjelpe private og offentlige virksomheter å jobbe mer datadrevet,
-		ønsker vi å forbedre situasjonen og hjelpe våre oppdragsgivere å skape konkurransefortrinn.
+		Sannsyn er et av Norges ledende fagmiljøer innen data science og maskinlæring. Vi har siden
+		starten i 2012 hjulpet en rekke virksomheter å skape konkurransekraft med egne data.
+	</p>
+	<p>
+		Vi har bred erfaring med alt fra å analysere data med fokus på å finne hvordan virksomheten kan
+		kommersialisere dataenes potensiale, til utvikling av maskinlæringsmodeller og etablering av
+		arkitektur som understøtter best mulig innsikt.
+	</p>
+	<p>
+		Et vellykket data science prosjekt krever en kombinasjon av solid forretningsforståelse, tung
+		matematisk/statistisk kompetanse og dyktige utviklere. Dette er en kombinasjon som er vanskelig
+		å få tak i, men som vi kan levere. Vi var tidlig ute i det norske markedet med fokus på data
+		science og maskinlæring, og har derfor rukket å få lang erfaring fra de fleste bransjer på dette
+		området.
+	</p>
+	<p>
+		I tillegg til å brenne for maskinlæring og stordata, så er vi opptatt av å at alle våre kunder
+		skal kunne dra nytte av vår kollektive kompetanse. Det gjør vi ved å jobbe systematisk med
+		kompetansedeling og ved å jobbe aktivt for å spille hverandre gode. Dette skaper trygge
+		konsulenter, godt samarbeidsklima og bedre sluttresultat.
 	</p>
 	<p>Les mer om oss her: <a {href}>{href}</a></p>
 	<h3>Fagområder</h3>
 	<p>
 		<strong>
-			Datavisualisering, datastrategi, dataarkitektur, foredrag, backend-utvikling, prediksjon &
-			dataanalyse og personalisering.
+			Maskinlæring, AI, store data / big data, Python, C#, Java, Kotlin, Spark, data-strategi,
+			data-infrastruktur, skyløsninger (AWS, Azure, GCP), Solr, søk / semantisk søk, tekstforståelse
+			/ entiteter i tekst
 		</strong>
 	</p>
 </div>
