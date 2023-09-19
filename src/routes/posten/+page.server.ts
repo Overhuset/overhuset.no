34be221b04325d35a9cf1145632afb76b5435a5f
Marius Kluften <mkluften93@gmail.com>
2023-09-19 15:01:01 +0200

    add separate posten page

---
+++ src/routes/posten/+page.server.ts
@@ -0,0 +1,18 @@
+import { SHEET_ID } from '$env/static/private';
+import { dev } from '$app/environment';
+import { getEventsFromSheet } from '$lib/getEventsFromSheet';
+
+export async function load() {
+	const events = await getEventsFromSheet(SHEET_ID);
+	return {
+		...events
+	};
+}
+
+export const config = dev
+	? {}
+	: {
+			isr: {
+				expiration: 60
+			}
+	  };
