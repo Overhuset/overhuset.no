d43f6d8c03cbbac1fc19a2fa51aacaa06ee046f4
Marius Kluften <marius@kluften.dev>
2023-11-15 11:15:43 +0100

    move all web related routes to web group

--- src/routes/(web)/+layout.svelte
+++ src/routes/(web)/+layout.svelte
@@ -1,7 +1,7 @@
 <script>
 	import Footer from '$lib/components/Footer.svelte';
-	import Header from '$lib/components/header.svelte';
-	import '../app.css';
+	import Header from '$lib/components/Header.svelte';
+	import '../../app.css';
 	import { initializeStores } from '@skeletonlabs/skeleton';
 
 	initializeStores();
