4e4498c9f798c78d3f9d839186600df6cd4f70b6
Marius Kluften <marius@kluften.dev>
2023-11-16 15:02:36 +0100

    create 404 page that checks url for displaying correct layout

---
+++ src/routes/+error.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	import Footer from '$lib/components/Footer.svelte';
+	import Header from '$lib/components/Header.svelte';
+	import IntranettHeader from '$lib/components/intranett/IntranettHeader.svelte';
+	import { initializeStores } from '@skeletonlabs/skeleton';
+	import '../app.css';
+
+	initializeStores();
+	const isIntranett = $page.url.href.includes('intranett');
+	const isWeb = !isIntranett;
+</script>
+
+{#if isWeb}
+	<Header />
+	<h1>Whoops</h1>
+	<Footer />
+{:else if isIntranett}
+	<IntranettHeader />
+	<h1>Intra whoops</h1>
+{/if}
