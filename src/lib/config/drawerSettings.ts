0c952027ce2dd797b875903613e1e8768125328f
Marius Kluften <marius@kluften.dev>
2023-11-16 15:10:16 +0100

    shared settings for drawer

---
+++ src/lib/config/drawerSettings.ts
@@ -0,0 +1,11 @@
+import type { DrawerSettings } from '@skeletonlabs/skeleton';
+
+export const drawerSettings: DrawerSettings = {
+	id: 'example-3',
+	// Provide your property overrides:
+	bgDrawer: 'bg-slate-900 text-white',
+	bgBackdrop: 'bg-gradient-to-tr from-black/50 via-blue-500/50 to-white/50',
+	width: 'w-[280px] md:w-[480px]',
+	padding: 'p-4',
+	rounded: 'rounded-xl'
+};
