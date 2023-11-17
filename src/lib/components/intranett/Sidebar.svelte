9e91eccbbe24c650108929a40f309093455ff51a
Marius Kluften <marius@kluften.dev>
2023-11-17 16:27:11 +0100

    clean up sidebar a bit

--- src/lib/components/intranett/Sidebar.svelte
+++ src/lib/components/intranett/Sidebar.svelte
@@ -1,14 +1,21 @@
 <script lang="ts">
 	import { TreeView } from '@skeletonlabs/skeleton';
 	import OhTreeViewItem from './OHTreeViewItem.svelte';
-	import intranettItems from '$lib/config/intranettItems';
+	import { getTree } from '$lib/config/intranettNavigation';
+	import { onMount } from 'svelte';
+	import type { TreeItem } from '$lib/config/intranettItems';
+
+	let tree: TreeItem[] = [];
+	onMount(() => {
+		tree = getTree(false);
+	});
 </script>
 
 <TreeView class="text-2xl">
-	<div class="pt-8 pl-8">
-		<h1 class="text-4xl">Meny</h1>
+	<div class="pt-8">
+		<h1 class="text-4xl pl-8">Meny</h1>
 		<hr />
-		<a href="/intranett" class="mt-8 block underline">Hjem</a>
+		<a href="/intranett" class="mt-8 pl-4 block underline">Hjem</a>
 	</div>
-	<OhTreeViewItem items={intranettItems} />
+	<OhTreeViewItem items={tree} />
 </TreeView>
