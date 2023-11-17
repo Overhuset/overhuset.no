8a6d1038b03c69c6a09f62db0b9e80ea87b71255
Marius Kluften <marius@kluften.dev>
2023-11-17 16:29:22 +0100

    improve error page for web page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
-	import { page } from '$app/stores';
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
 	import IntranettHeader from '$lib/components/intranett/IntranettHeader.svelte';
-	import { initializeStores } from '@skeletonlabs/skeleton';
+	import { Drawer, initializeStores } from '@skeletonlabs/skeleton';
 	import '../app.css';
+	import Sidebar from '$lib/components/intranett/Sidebar.svelte';
+	import { page } from '$app/stores';
 
 	initializeStores();
 	const isIntranett = $page.url.href.includes('intranett');
@@ -13,9 +14,17 @@
 
 {#if isWeb}
 	<Header />
-	<h1>Whoops</h1>
+	<div class="w-full flex flex-col gap-8 items-center my-auto">
+		<h1 class="h1 text-5xl">404</h1>
+		<p class="text-3xl">Fant ikke denne siden gitt</p>
+		<a class="text-xl underline" href="/">Gå tilbake til start og prøv igjen</a>
+	</div>
 	<Footer />
 {:else if isIntranett}
 	<IntranettHeader />
-	<h1>Intra whoops</h1>
+	<Drawer>
+		<Sidebar />
+	</Drawer>
+
+	<slot />
 {/if}
