78bb5ab2fa823874e3f0fdfefab9f2a85ca907a6
Marius Kluften <marius@kluften.dev>
2023-11-17 16:26:35 +0100

    update styling

--- src/lib/config/drawerSettings.ts
+++ src/lib/config/drawerSettings.ts
@@ -3,9 +3,9 @@ import type { DrawerSettings } from '@skeletonlabs/skeleton';
 export const drawerSettings: DrawerSettings = {
 	id: 'example-3',
 	// Provide your property overrides:
-	bgDrawer: 'bg-slate-900 text-white',
-	bgBackdrop: 'bg-gradient-to-tr from-black/50 via-blue-500/50 to-white/50',
-	width: 'w-[280px] md:w-[480px]',
+	bgDrawer: 'bg-surface-900 text-white',
+	bgBackdrop: 'bg-gradient-to-tr from-black/50 via-surface-500/50 to-white/50',
+	width: 'w-1/2 md:w-1/2',
 	padding: 'p-4',
 	rounded: 'rounded-xl'
 };
