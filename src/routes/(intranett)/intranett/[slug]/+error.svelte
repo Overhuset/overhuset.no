7dd31b2f984f7b14d6711be46cb247ca3cd972f6
Marius Kluften <marius@kluften.dev>
2023-11-18 11:20:18 +0100

    add drawer to error page as well

--- src/routes/(intranett)/intranett/[slug]/+error.svelte
+++ src/routes/(intranett)/intranett/[slug]/+error.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
 	import { page } from '$app/stores';
+	import Sidebar from '$lib/components/intranett/Sidebar.svelte';
+	import { Drawer } from '@skeletonlabs/skeleton';
 </script>
 
 <div class="dark w-full flex flex-col gap-8 items-center my-auto mt-20">
@@ -10,3 +12,7 @@
 		<a class=" underline" href="/intranett"> gå tilbake til start og prøv igjen</a>
 	</p>
 </div>
+
+<Drawer>
+	<Sidebar />
+</Drawer>
