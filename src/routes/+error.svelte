36132ceab2c34a62a329b4644091762036751645
Marius Kluften <marius@kluften.dev>
2023-11-18 11:20:34 +0100

    rely on awesome slot based error pages

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
 	import Footer from '$lib/components/Footer.svelte';
 	import Header from '$lib/components/Header.svelte';
-	import IntranettHeader from '$lib/components/intranett/IntranettHeader.svelte';
-	import { Drawer, initializeStores } from '@skeletonlabs/skeleton';
+	import { initializeStores } from '@skeletonlabs/skeleton';
 	import '../app.css';
-	import Sidebar from '$lib/components/intranett/Sidebar.svelte';
 	import { page } from '$app/stores';
 
 	initializeStores();
-	const isIntranett = $page.url.href.includes('intranett');
+	const isIntranett = $page.url.href.split('/').includes('intranett');
 	const isWeb = !isIntranett;
 </script>
 
@@ -21,10 +19,5 @@
 	</div>
 	<Footer />
 {:else if isIntranett}
-	<IntranettHeader />
-	<Drawer>
-		<Sidebar />
-	</Drawer>
-
 	<slot />
 {/if}
