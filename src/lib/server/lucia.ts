b369852ac5676e37fe39a7e4ce265b45e4b3c23e
Anndrea <anndrea@wanda.space>
2023-11-28 12:10:30 +0100

    initial google login, callback not done

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -3,8 +3,13 @@ import { lucia } from 'lucia';
 import { sveltekit } from 'lucia/middleware';
 import { dev } from '$app/environment';
 
-import { github } from '@lucia-auth/oauth/providers';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+import { github, google } from '@lucia-auth/oauth/providers';
+import {
+	GITHUB_CLIENT_ID,
+	GITHUB_CLIENT_SECRET,
+	GOOGLE_CLIENT_ID,
+	GOOGLE_CLIENT_SECRET
+} from '$env/static/private';
 import { pg } from '@lucia-auth/adapter-postgresql';
 import { db } from '@vercel/postgres';
 
@@ -16,9 +21,14 @@ export const auth = lucia({
 	middleware: sveltekit(),
 	getUserAttributes: (data) => {
 		return {
-			githubUsername: data.username
+			username: data.username
 		};
 	}
+});
+export const googleAuth = google(auth, {
+	clientId: GOOGLE_CLIENT_ID,
+	clientSecret: GOOGLE_CLIENT_SECRET,
+	redirectUri: 'http://localhost:5173/intranett/login/google/callback'
 });
 
 export const githubAuth = github(auth, {
--- src/routes/(intranett)/intranett/login/+page.svelte
+++ src/routes/(intranett)/intranett/login/+page.svelte
@@ -1,4 +1,7 @@
 <div>
-	<h1>Please sign in</h1>
+	<h1>Please sign in with GH</h1>
 	<a href="/intranett/login/github">Sign in with GH</a>
+
+		<h1>Please sign in with Google</h1>
+	<a href="/intranett/login/google">Sign in with Google</a>
 </div>
