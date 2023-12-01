3ebfb67d05d0c60526eda15ca7565da6afc475e2
Anndrea <anndrea@wanda.space>
2023-12-01 11:13:38 +0100

    google login and logout, needs styling and love

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -2,14 +2,13 @@ import { auth } from '$lib/server/lucia';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
-	console.log('event', event);
+	event.locals.auth = auth.handleRequest(event);
+
 	if (
 		event.url.pathname.startsWith('/intranett') &&
 		!event.url.pathname.startsWith('/intranett/login')
 	) {
-		event.locals.auth = auth.handleRequest(event);
 		const session = await event.locals.auth.validate();
-		console.log(session);
 		if (session) return await resolve(event);
 		return new Response('Redirect', { status: 303, headers: { Location: '/intranett/login' } });
 	}
--- src/routes/(intranett)/intranett/login/+page.svelte
+++ src/routes/(intranett)/intranett/login/+page.svelte
@@ -1,7 +1,6 @@
-<div>
-	<h1>Please sign in with GH</h1>
-	<a href="/intranett/login/github">Sign in with GH</a>
 
+<div>
 		<h1>Please sign in with Google</h1>
 	<a href="/intranett/login/google">Sign in with Google</a>
+	
 </div>
