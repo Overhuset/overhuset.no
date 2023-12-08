fc8f43e6d6161ed301dea9e53ecf18bc49cc0ef4
Marius Kluften <marius@kluften.dev>
2023-12-08 11:21:08 +0100

    change redirect url to be env var instead

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -4,7 +4,7 @@ import { sveltekit } from 'lucia/middleware';
 import { dev } from '$app/environment';
 
 import { google } from '@lucia-auth/oauth/providers';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET } from '$env/static/private';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI } from '$env/static/private';
 import { pg } from '@lucia-auth/adapter-postgresql';
 import { db } from '@vercel/postgres';
 
@@ -22,13 +22,11 @@ export const auth = lucia({
 });
 
 // Get full path to web app anywhere it is. If it's not on Vercel, default to local host
-const vercel_url = process.env.VERCEL_URL;
-const redirectUriBase = vercel_url ? `https://${vercel_url}` : 'http://localhost:5173';
 
 export const googleAuth = google(auth, {
 	clientId: GOOGLE_CLIENT_ID,
 	clientSecret: GOOGLE_CLIENT_SECRET,
-	redirectUri: `${redirectUriBase}/intranett/login/google/callback`,
+	redirectUri: GOOGLE_REDIRECT_URI,
 	scope: ['email']
 });
 
