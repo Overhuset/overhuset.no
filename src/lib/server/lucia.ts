b257cf50f95652837e561f66661e2ce2c06cc1a6
Roger A. Slaaen <roger@jpro.no>
2023-12-27 12:27:43 +0100

    using system variable and env

--- src/lib/server/lucia.ts
+++ src/lib/server/lucia.ts
@@ -4,7 +4,7 @@ import { sveltekit } from 'lucia/middleware';
 import { dev } from '$app/environment';
 
 import { google, azureAD } from '@lucia-auth/oauth/providers';
-import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, GOOGLE_REDIRECT_URI, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT, AZURE_REDIRECT_URI } from '$env/static/private';
+import { GOOGLE_CLIENT_ID, GOOGLE_CLIENT_SECRET, AZURE_CLIENT_ID, AZURE_CLIENT_SECRET, AZURE_TENANT, REDIRECT_URI } from '$env/static/private';
 import { pg } from '@lucia-auth/adapter-postgresql';
 import { db } from '@vercel/postgres';
 
@@ -22,13 +22,12 @@ export const auth = lucia({
 });
 
 // Get full path to web app anywhere it is. If it's not on Vercel, default to local host
-const vercel_url = process.env.VERCEL_URL;
-const redirectUriBase = vercel_url ? `https://${vercel_url}` : 'http://localhost:5173';
+const redirectUriBase =  REDIRECT_URI ? REDIRECT_URI : `https://${process.env.VERCEL_URL}`;
 
 export const googleAuth = google(auth, {
 	clientId: GOOGLE_CLIENT_ID,
 	clientSecret: GOOGLE_CLIENT_SECRET,
-	redirectUri: GOOGLE_REDIRECT_URI,
+	redirectUri: `${redirectUriBase}/intranett/login/google/callback`,
 	scope: ['email']
 });
 
@@ -36,7 +35,6 @@ export const azureADAuth = azureAD(auth, {
 	clientId: AZURE_CLIENT_ID,
 	clientSecret: AZURE_CLIENT_SECRET,
 	tenant: AZURE_TENANT,
-	// redirectUri: AZURE_REDIRECT_URI,
 	redirectUri: `${redirectUriBase}/intranett/login/microsoft/callback`,
 	scope: ['email']
 });
