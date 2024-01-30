480ed4ec37fab0da20cdd9632575e69ea750d35d
Roger A. Slaaen <roger@jpro.no>
2024-01-30 11:05:55 +0100

    Add new page for movies

---
+++ src/routes/(web)/kino/+page.svelte
@@ -0,0 +1,15 @@
+<script>
+import { MetaTags } from 'svelte-meta-tags';
+</script>
+<section class="max-w-6xl mx-auto md:w-4/5">
+    <MetaTags
+	title='Konsulentselskapet Overhuset – inviterer til kino!'
+	description='Overhuset inviterer til førpremieren på Dune II.'
+    />
+	<div class="prose mb-16 mt-8 mx-4">
+		<h1>Kino</h1>
+        <p class="font-semibold">
+        TBA
+        </p>
+	</div>
+</section>
