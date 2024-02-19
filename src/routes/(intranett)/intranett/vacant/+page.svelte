f785e2e658b0bf73d931b14e26f7606cb8294254
Petter <pettermailyahoo.com>
2024-02-19 09:52:26 +0100

    style fileupload form

--- src/routes/(intranett)/intranett/vacant/+page.svelte
+++ src/routes/(intranett)/intranett/vacant/+page.svelte
@@ -13,7 +13,9 @@
 	const api = '/api/vacant';
 	const headers = {'content-type': 'application/json'};
  	let newVacant: Vacant | undefined = undefined;
+	let fileSelect: string | undefined = undefined;
 	let isUploadingCV: boolean = false;
+
 
 	 const sortVacantList = (by: "firstName" | "vacantFrom" | "createdAt") => {
 		 const clone = [...data.vacantList];
@@ -25,9 +27,18 @@
 		 }
 	 }
 
+	 const getCvShortName = () => {
+		 if (fileSelect) {
+			const split = fileSelect.split("\\");
+ 			return split[split.length-1];
+		 }
+		 return null;
+	}
+
 	const onCloseForm = () => {
 		open = false;
 		newVacant = undefined;
+		fileSelect = undefined;
 		isUploadingCV = false;
 		if (form) {
 			form.uploaded = "";
@@ -83,51 +94,53 @@
 		<div>
 			{#if !open}
 				<div class="buttons-container">
-					<button on:click={handleToggleNewForm} class="cursor-pointer rounded-lg bg-white px-2 py-1"> Legg til </button>
+					<button on:click={handleToggleNewForm} class="cursor-pointer rounded-lg bg-white px-2 py-1"> Legg til konsulent</button>
 				</div>
 			{/if}
 
 			<div use:collapse={{open}}>
 				{#if !!newVacant}
 					<h3>Registrer ledig konsulent</h3>
+
 					<form on:submit|preventDefault={handleNewEntry}>
-						<table>
-							<tr>
-								<td><label for="name">Navn</label></td>
-								<td><input name="name" id="name" type="text" bind:value={newVacant.name}/></td>
-							</tr>
-							<tr>
-								<td><label for="from">Ledig fra</label></td>
-								<td><input name="from" id="from" type="date" bind:value={newVacant.vacantFrom}  /></td>
-							</tr>
-							<tr>
-								<td><label for="comment">Kommentar</label></td>
-								<td><textarea bind:value={newVacant.comment}  /></td>
-							</tr>
-							<tr>
-								<td>
-									<label for="cv">
-										CV {#if form?.uploaded} (lastet inn) {/if}
-									</label>
-								</td>
-								<td>
-									<form use:enhance action="?/upload" method="POST" enctype="multipart/form-data">
- 										<input type="file" required name="cv" id="cv" class="cvInput"/>
-										<button class="secondaryButton" on:click={handleUploadStart}>
-											{#if !form?.uploaded && isUploadingCV} laster... {:else} Last opp {/if}
-										</button>
-									</form>
-								</td>
-							</tr>
-						</table>
+						<label for="name">Navn *</label>
+						<input name="name" id="name" type="text" bind:value={newVacant.name}/>
+						<label for="from">Ledig fra *</label>
+						<input name="from" id="from" type="date" bind:value={newVacant.vacantFrom}/>
+						<label for="comment">Kommentar</label>
+						<textarea bind:value={newVacant.comment}/>
+
+						<form use:enhance action="?/upload" method="POST" enctype="multipart/form-data">
+							<label>CV - filen kan være på maks 4.5mb</label>
+							<input type="file" bind:value={fileSelect} required name="cv" id="cv" class="cvInput"/>
+
+							<div class="file-upload-container">
+								<button class="secondaryButton" onClick="document.getElementById('cv').click();" style="min-width: 8rem">Velg en fil</button>
+								<div class="file-select-container">
+									{#if fileSelect}
+										<span>{getCvShortName()}</span>
+										{#if !form?.uploaded}
+											<button class="primaryButton" style="margin-left: 1rem" on:click={handleUploadStart}>
+												{#if isUploadingCV}laster...{:else}Last opp{/if}
+											</button>
+										{:else}
+											(lastet opp)
+										{/if}
+									{/if}
+								</div>
+							</div>
+						</form>
+
+						<br/>
 
 						<div class="buttons-container">
-							<button class="secondaryButton" on:click={handleToggleNewForm}> Avbryt </button>
+							<button class="secondaryButton" on:click={handleToggleNewForm}>Avbryt</button>
 							<button class="primaryButton" disabled={
 								!newVacant.name ||
 								!newVacant.vacantFrom ||
-								!newVacant.createdBy
-							}> Legg til </button>
+								!newVacant.createdBy ||
+								(fileSelect && !form?.uploaded)
+							}>Registrer konsulent</button>
 						</div>
 					</form>
 				{/if}
@@ -155,20 +168,28 @@
 		padding: 0.2rem 0.4rem;
 		margin-bottom: 0.4rem;
 	}
+	input[type="file"] {
+		display:none;
+	}
+	input[type="text"]{
+		width: 70%;
+	}
 	label {
+		margin-top: 0.3rem;
 		margin-right: 2rem;
 	}
 	textarea {
 		border: 1px solid #ababab;
 		border-radius: 0.3rem;
 		width: 100%;
-		min-height: 10rem;
+		min-height: 9rem;
 		padding: 0.2rem 0.4rem;
 	}
 	button {
 		padding: 0.2rem 0.8rem;
 		border-radius: 0.5rem;
 	}
+
 	.layout {
 		width: 100%;
 		max-width: 70rem;
@@ -195,6 +216,18 @@
 		display: flex;
 		justify-content: flex-end;
 		gap: 1rem;
+	}
+	.file-upload-container {
+		width: 100%;
+		display: flex;
+		justify-content: space-between;
+		gap: 1rem;
+	}
+	.file-select-container {
+		width: 100%;
+		display: flex;
+		justify-content: flex-end;
+		align-items: center;
 	}
 	.cvInput {
 		background-color: white;
