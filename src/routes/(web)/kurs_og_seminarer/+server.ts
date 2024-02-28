5004ee6ba4defb159cf19323a75a02662534bb16
Hansen <steinar@jpro.no>
2024-02-28 11:27:21 +0100

    Lagt inn en liten "debug" console log for å se om koden trigges.

--- src/routes/(web)/kurs_og_seminarer/+server.ts
+++ src/routes/(web)/kurs_og_seminarer/+server.ts
@@ -3,6 +3,7 @@ import type { RequestHandler } from '@sveltejs/kit';
 export const GET: RequestHandler = async ({ url }) => {
     // Sjekker om URL-en ender med '/'
     if (url.pathname.endsWith('/')) {
+        console.log("Redirecting to", url.pathname.slice(0, -1));
         const newLocation = url.pathname.slice(0, -1); // Fjerner skråstrek fra slutten
         return new Response(undefined, {
             status: 301, // Permanent redirect, bruk 302 for en midlertidig redirect hvis det er ønskelig
