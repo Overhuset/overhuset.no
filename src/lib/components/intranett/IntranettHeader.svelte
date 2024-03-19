7d06a6b0d4a30fdfebdb2a1f18f2e332bf7a269a
Petter <pettermailyahoo.com>
2024-03-19 23:28:29 +0100

    fix hamburger

--- src/lib/components/intranett/IntranettHeader.svelte
+++ src/lib/components/intranett/IntranettHeader.svelte
@@ -9,6 +9,7 @@
 
 	let tree: TreeItem[] = [];
 	export let loggedIn: boolean = false;
+
 	onMount(() => {
 		if (loggedIn) tree = getTree(!dev);
 		else tree = [];
@@ -16,14 +17,16 @@
 </script>
 
 <AppBar slotLead="text-2xl">
+
 	<svelte:fragment slot="lead">
-		<div class="flex gap-4">
+		<div class="flex gap-4" style="padding-left: 1rem">
 			{#if loggedIn}
-				<button class="md:hidden" on:click={() => drawerStore.open(drawerSettings)}>🍔</button>
+				<button class="md:hidden" on:click={() => drawerStore.open(drawerSettings)}>☰</button>
 			{/if}
 			<a href="/intranett" class="text-white">Overhusets Intranett</a>
 		</div>
 	</svelte:fragment>
+
 	<div class="hidden md:flex text-white gap-4 text-xl justify-end">
 		{#each tree as item}
 			{#if isLinkItem(item)}
@@ -35,8 +38,10 @@
 
 		{#if loggedIn}
 			<a href={`/intranett/ledig`}>Ledige konsulenter</a>
+			<a href={`/intranett/vedlikehold_kurs_og_seminarer`}>Administrer</a>
 		{/if}
 	</div>
+
 	<svelte:fragment slot="trail">
 		{#if loggedIn}
 			<form method="post" action="/intranett?/logout" use:enhance>
@@ -44,4 +49,5 @@
 			</form>
 		{/if}
 	</svelte:fragment>
+
 </AppBar>
--- src/lib/components/intranett/Sidebar.svelte
+++ src/lib/components/intranett/Sidebar.svelte
@@ -16,5 +16,5 @@
 <TreeView class="text-2xl">
 	<h1 class="text-4xl pt-8 pl-8">Meny</h1>
 	<hr class="border-[2px] border-white divider" />
-	<OhTreeViewItem items={tree} />
+	<OhTreeViewItem items={tree} loggedIn={loggedIn}/>
 </TreeView>
