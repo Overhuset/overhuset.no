ba803b9c19a98e8d604cf7aa9b378f9024c26638
Hansen <steinar@jpro.no>
2024-10-09 13:28:24 +0200

    Update +page.server.ts

--- src/routes/(intranett)/intranett/[slug]/+page.server.ts
+++ src/routes/(intranett)/intranett/[slug]/+page.server.ts
@@ -1,26 +1,32 @@
+import fs from 'fs';
+import path from 'path';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { getLinkItemBySlug }  from '$lib/config/intranett/routes';
+import { getLinkItemBySlug } from '$lib/config/intranett/routes';
 import { accessCheck } from '$lib/utils/accessController';
 import { createPool } from '@vercel/postgres';
 import { fetchAuthUser } from '$lib/data-access/user';
 
+export const load: PageServerLoad = async ({ fetch, params, locals }) => {
+  const db = createPool();
+  const session = await locals.auth.validate();
+  const user = session?.user;
+  const authUser = user?.userId ? await fetchAuthUser(db, user.userId) : undefined;
 
-export const load: PageServerLoad = async ({ fetch, params,locals }) => {
-	const db = createPool();
-	const session = await locals.auth.validate();
-	const user  = session?.user;
-	const authUser = user?.userId ? await fetchAuthUser(db, user.userId) : undefined;
+  await accessCheck(db, authUser, `/intranett/${params.slug}`);
 
-	await accessCheck(db,  authUser, `/intranett/${params.slug}`);
+  const item = getLinkItemBySlug(params.slug);
 
-	const item = getLinkItemBySlug(params.slug);
-	const post = await fetch(`/intranett-articles/articles/${item?.markdown}`);
+  if (!item) {
+    throw error(404, { message: 'Fant ikke element for slug: ' + params.slug });
+  }
 
-	if (post.status === 404) {
-		throw error(404, { message: 'Vi fant ikke denne siden.' });
-	}
+  // Bruk fs til å lese filen direkte
+  const filePath = path.resolve(`static/intranett-articles/articles/${item.markdown}`);
+  if (!fs.existsSync(filePath)) {
+    throw error(404, { message: 'Vi fant ikke denne siden.' });
+  }
 
-	const content = await post.text();
-	return { content };
+  const markdownContent = fs.readFileSync(filePath, 'utf-8');
+  return { content: markdownContent };
 };
