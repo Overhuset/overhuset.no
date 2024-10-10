79f33183ccfad1b0180bb900eea5f6efa797e22f
steinarhansen <steinar@jpro.no>
2024-10-10 13:02:38 +0200

    Flyttet md filene til src/lib/content/articles for mer ryddig håndtering ved bygging.

--- src/routes/(intranett)/intranett/[slug]/+page.server.ts
+++ src/routes/(intranett)/intranett/[slug]/+page.server.ts
@@ -1,5 +1,3 @@
-import fs from 'fs';
-import path from 'path';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { getLinkItemBySlug } from '$lib/config/intranett/routes';
@@ -7,7 +5,9 @@ import { accessCheck } from '$lib/utils/accessController';
 import { createPool } from '@vercel/postgres';
 import { fetchAuthUser } from '$lib/data-access/user';
 
-export const load: PageServerLoad = async ({ fetch, params, locals }) => {
+const allMarkdownFiles = import.meta.glob('/src/lib/content/articles/*.md', { as: 'raw' });
+
+export const load: PageServerLoad = async ({ params, locals }) => {
   const db = createPool();
   const session = await locals.auth.validate();
   const user = session?.user;
@@ -21,12 +21,13 @@ export const load: PageServerLoad = async ({ fetch, params, locals }) => {
     throw error(404, { message: 'Fant ikke element for slug: ' + params.slug });
   }
 
-  // Bruk fs til å lese filen direkte
-  const filePath = path.resolve(`static/intranett-articles/articles/${item.markdown}`);
-  if (!fs.existsSync(filePath)) {
+  const fileKey = `/src/lib/content/articles/${item.markdown}`;
+  const loader = allMarkdownFiles[fileKey];
+
+  if (!loader) {
     throw error(404, { message: 'Vi fant ikke denne siden.' });
   }
 
-  const markdownContent = fs.readFileSync(filePath, 'utf-8');
+  const markdownContent = await loader();
   return { content: markdownContent };
 };
