be59949119e8b532294eed317ec96d222766f5f2
steinarhansen <steinar@jpro.no>
2024-10-10 13:30:21 +0200

    Forsøk på fiks for å hente index.md riktig.

--- src/routes/(intranett)/intranett/[...slug]/+page.server.ts
+++ src/routes/(intranett)/intranett/[...slug]/+page.server.ts
@@ -1,3 +1,5 @@
+import fs from 'fs';
+import path from 'path';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
 import { getLinkItemBySlug } from '$lib/config/intranett/routes';
@@ -5,29 +7,35 @@ import { accessCheck } from '$lib/utils/accessController';
 import { createPool } from '@vercel/postgres';
 import { fetchAuthUser } from '$lib/data-access/user';
 
-const allMarkdownFiles = import.meta.glob('/src/lib/content/articles/*.md', { as: 'raw' });
-
 export const load: PageServerLoad = async ({ params, locals }) => {
   const db = createPool();
   const session = await locals.auth.validate();
   const user = session?.user;
   const authUser = user?.userId ? await fetchAuthUser(db, user.userId) : undefined;
 
-  await accessCheck(db, authUser, `/intranett/${params.slug}`);
+  const slugArray = params.slug || [];
+  const slug = slugArray.join('/');
 
-  const item = getLinkItemBySlug(params.slug);
+  console.log('Slug:', slug);
+
+  await accessCheck(db, authUser, `/intranett/${slug}`);
+
+  const item = getLinkItemBySlug(slug);
+
+  console.log('Item:', item);
 
   if (!item) {
-    throw error(404, { message: 'Fant ikke element for slug: ' + params.slug });
+    throw error(404, { message: 'Fant ikke element for slug: ' + slug });
   }
 
-  const fileKey = `/src/lib/content/articles/${item.markdown}`;
-  const loader = allMarkdownFiles[fileKey];
+  const filePath = path.resolve(`src/lib/content/articles/${item.markdown}`);
 
-  if (!loader) {
+  console.log('File path:', filePath);
+
+  if (!fs.existsSync(filePath)) {
     throw error(404, { message: 'Vi fant ikke denne siden.' });
   }
 
-  const markdownContent = await loader();
+  const markdownContent = fs.readFileSync(filePath, 'utf-8');
   return { content: markdownContent };
 };
