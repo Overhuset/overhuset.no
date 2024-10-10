22002a1797f65c11e7fedf7feee6cef87837ec7d
steinarhansen <steinar@jpro.no>
2024-10-10 14:23:36 +0200

    forsøk på fiks av feil ved uthenting av md-filer.

--- src/routes/(intranett)/intranett/[...slug]/+page.server.ts
+++ src/routes/(intranett)/intranett/[...slug]/+page.server.ts
@@ -1,50 +1,26 @@
-import fs from 'fs';
-import path from 'path';
 import { error } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
-import { getLinkItemBySlug } from '$lib/config/intranett/routes';
+import { getLinkItemBySlug }  from '$lib/config/intranett/routes';
 import { accessCheck } from '$lib/utils/accessController';
 import { createPool } from '@vercel/postgres';
 import { fetchAuthUser } from '$lib/data-access/user';
 
-export const load: PageServerLoad = async ({ params, locals }) => {
-  const db = createPool();
-  const session = await locals.auth.validate();
-  const user = session?.user;
-  const authUser = user?.userId ? await fetchAuthUser(db, user.userId) : undefined;
 
-  // Sjekk om slug er en array, og hvis ikke, pakk den inn i en array
-  const slugArray = Array.isArray(params.slug) ? params.slug : [params.slug];
-  const slug = slugArray.join('/');
+export const load: PageServerLoad = async ({ fetch, params,locals }) => {
+	const db = createPool();
+	const session = await locals.auth.validate();
+	const user  = session?.user;
+	const authUser = user?.userId ? await fetchAuthUser(db, user.userId) : undefined;
 
-  console.log('Slug:', slug);
+	await accessCheck(db,  authUser, `/intranett/${params.slug}`);
 
-  // Sjekk brukerens tilgang til siden
-  await accessCheck(db, authUser, `/intranett/${slug}`);
+	const item = getLinkItemBySlug(params.slug);
+	const post = await fetch(`/intranett-articles/articles/${item?.markdown}`);
 
-  // Hent riktig element basert på slug
-  const item = getLinkItemBySlug(slug);
+	if (post.status === 404) {
+		throw error(404, { message: 'Vi fant ikke denne siden.' });
+	}
 
-  console.log('Item:', item);
-
-  // Hvis elementet ikke finnes, kast en 404-feil
-  if (!item) {
-    throw error(404, { message: 'Fant ikke element for slug: ' + slug });
-  }
-
-  // Definer stien til markdown-filen for innholdet
-  const filePath = path.resolve(`static/intranett-articles/articles/${item.markdown}`);
-
-  console.log('File path:', filePath);
-
-  // Sjekk om filen eksisterer
-  if (!fs.existsSync(filePath)) {
-    throw error(404, { message: 'Vi fant ikke denne siden.' });
-  }
-
-  // Les innholdet fra markdown-filen
-  const markdownContent = fs.readFileSync(filePath, 'utf-8');
-
-  // Returner markdown-innholdet
-  return { content: markdownContent };
+	const content = await post.text();
+	return { content };
 };
